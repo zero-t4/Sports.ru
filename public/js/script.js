46d3f0bf0d3ecc8e21abc61b2d259dbba73ec162
Nadir Zh <fobasip@gmail.com>
2016-12-01 22:40:28 +0300

    commint

---
+++ public/js/script.js
@@ -0,0 +1,41 @@
+window.onload = function() {
+  let url = 'js/seriea.json';
+  
+  function sendAjax(url) {
+    return new Promise((resolve, reject) => {
+      let xhr = new XMLHttpRequest();
+      
+      xhr.open('GET', url);
+      xhr.addEventListener('load', () => {
+        resolve(xhr.responseText);
+      });
+      xhr.addEventListener('error', () => {
+        reject();
+      });
+      xhr.send();
+    });
+  }
+  sendAjax(url).then(
+    responseText => {
+      responseText = JSON.parse(responseText);
+      
+      for (let key in responseText.teams) {
+        if(responseText.teams[key].flag_country == "Италия") {
+          responseText.teams[key].flag_country = "italy"
+        }
+      }
+      
+      show();
+      function show() {
+        let source = temp.innerHTML;
+        let templateFn = Handlebars.compile(source);
+        let template = templateFn({responseText: responseText.teams});
+    
+        results.innerHTML = template;
+      }
+    }, 
+    () => {
+      alert('Some error occurred');
+    }
+  )
+}
